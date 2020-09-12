c7bace9612f62c48347b6334bd8da98f1d747d2e
linocrusher <egrelayson@up.edu.ph>
2020-09-12 16:15:41 +0800

    Module 5.

--- SpeakGoodBye.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  var speakWord = "Good Bye";
-  var byeSpeaker = {
-    speak: function() {
-      console.log(speakWord + " " + this.name);
-    }
-  }
-  window.byeSpeaker = byeSpeaker;
-})();
--- SpeakHello.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  var speakWord = "Hello";
-  var helloSpeaker = {
-    speak: function() {
-      console.log(speakWord + " " + this.name);
-    }
-  }
-  window.helloSpeaker = helloSpeaker;
-})();
